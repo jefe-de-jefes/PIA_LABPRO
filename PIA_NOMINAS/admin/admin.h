588607e4d1c32fb1be6ef6ac828261195c1b75d9
jefe-de-jefes <luissegovia141005@gmail.com>
2024-11-15 08:40:51 -0600

    modificando

--- PIA_NOMINAS/admin/admin.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdio.h>
-
-void listar_usuarios(FILE* archivo);
-void desbloquear_usuario(FILE* archivo);
-void anadir_usuario(FILE* archivo);
-void eliminar_usuario(FILE* archivo);
--- PIA_NOMINAS/admin/admin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-include_dir="./nomina/include"
-source="./admin/main.c ./admin/admin.c ./nomina/source/misc.c"
-$1 -o ./admin_app -iquote $include_dir $source
